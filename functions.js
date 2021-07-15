5bb0b7c26f4f6176a993d3bb2b25d7b77274bd95
Radulescu Alex <ralexandru89@yahoo.com>
2021-07-15 21:45:50 +0300

    sort skills

--- functions.js
+++ functions.js
@@ -61,15 +61,35 @@ function showSkills(skills){
     var skillsHtml = skills.map(function(skill){
         var favorit = skill.favorit ? 'class= favorit' : '';
         var endorsements = skill.endorsements > 5 ? `<span>${skill.endorsements}</span>` : '';
-        return `<li ${favorit}>${skill.name} ${endorsements}</li>`;
+        return `<li ${favorit}>${skill.name.toLowerCase()} ${endorsements}</li>`;
     }).join('');
     
     document.querySelector("#skills ul").innerHTML = skillsHtml;
 }
 
+function sortSkillsByName(a, b){
+    var aName = a.name.toUpperCase();
+    var bName = b.name.toUpperCase();
+    if (aName < bName) {
+        return -1;
+    }
+    if (aName > bName){
+    return 1;
+     }
+ 
+     return 0; 
+};
+
+function sortSkillsByEndorsements(a, b){
+    return b.endorsements - a.endorsements;
+}
+
 fetch("data/skills.json").then(function(response) {
     return response.json();
 }).then(function(skills){
+    skills.sort(sortSkillsByEndorsements);
     window.skills = skills;
     showSkills(skills);
 })
+
+
