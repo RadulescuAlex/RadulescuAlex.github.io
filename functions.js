76d69bc6263d0a303329ba8784bdb961abca19d1
Radulescu Alex <ralexandru89@yahoo.com>
2021-07-19 20:04:25 +0300

    remove () from single param

--- functions.js
+++ functions.js
@@ -34,7 +34,7 @@ function show(id){
 
 function hideAllPages() {
     const pages = Array.from(document.getElementsByClassName('page'));
-    pages.forEach((page) => {
+    pages.forEach(page => {
         hide(page.id);
     });
 }
@@ -46,7 +46,7 @@ function showPage(id) {
 
 show('skills');
 
-document.querySelector('#top-menu-bar').addEventListener('click', (e) =>{
+document.querySelector('#top-menu-bar').addEventListener('click', e =>{
     if(e.target.matches('a')) {
         const id = e.target.getAttribute('data-page');
         showPage(id);
@@ -58,7 +58,7 @@ document.querySelector('#top-menu-bar').addEventListener('click', (e) =>{
 window.skills = [];
 
 function showSkills(skills){
-    const skillsHtml = skills.map((skill) =>{
+    const skillsHtml = skills.map(skill =>{
         const favorit = skill.favorit ? 'class= favorit' : '';
         const endorsements = skill.endorsements > 5 ? `<span>${skill.endorsements}</span>` : '';
         return `<li ${favorit}>${skill.name.toLowerCase()} ${endorsements}</li>`;
